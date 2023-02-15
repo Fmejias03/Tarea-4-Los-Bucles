fee77769789df40cb91c7fe26b85ff1154bcf515
Fernando <fmeji@hotmail.es>
2023-02-16 00:03:41 +0100

    Ejercicio 1 V2

--- src/main/java/Ejercicio1.java
+++ src/main/java/Ejercicio1.java
@@ -23,7 +23,7 @@ public class Ejercicio1 {
     //
     //La media es 14,5
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         // TODO Auto-generated method stub
         int n;
         double nota, suma, media;
@@ -38,5 +38,46 @@ public class Ejercicio1 {
         }
         media = suma / n;
         System.out.println("La media es " + media);
+    }*/
+
+    //------------------------------------------------------------------------------------------------------------------
+
+    //Ejercicio 1 V2
+
+    //El objetivo sigue siendo realizar el cálculo de la media de un conjunto de valores introducidos por el usuario, pero el usuario ya no tiene que indicar inicialmente el número de valores. Introduce los valores y cuando ha terminado, introduce un valor concreto (-1), para indicar que ha terminado de introducir valores.
+    //
+    //Ejemplo de ejecución del algoritmo:
+    //
+    //¿Nota (-1 para terminar)?
+    //
+    //9
+    //
+    //¿Nota (-1 para terminar)?
+    //
+    //18
+    //
+    //¿Nota (-1 para terminar)?
+    //
+    //-1
+    //
+    //La nota media es 13,5
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        double nota, suma, media;
+        int n;
+        Scanner teclado = new Scanner(System.in);
+        suma = 0;
+        n = 0;
+        System.out.println("¿Nota (-1 para terminar)?");
+        nota = teclado.nextDouble();
+        while (nota != -1) {
+            suma = suma + nota;
+            n = n + 1;
+            System.out.println("¿Nota (-1 para terminar)?");
+            nota = teclado.nextDouble();
+        }
+        media = suma / n;
+        System.out.println("La nota media es " + media);
     }
 }
