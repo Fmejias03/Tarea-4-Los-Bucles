56d754427ec5da8f055f1599cb09a3c132e81c8c
Fernando <fmeji@hotmail.es>
2023-02-16 00:08:50 +0100

    Ejercicio 1 V3

--- src/main/java/Ejercicio1.java
+++ src/main/java/Ejercicio1.java
@@ -62,7 +62,7 @@ public class Ejercicio1 {
     //
     //La nota media es 13,5
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         // TODO Auto-generated method stub
         double nota, suma, media;
         int n;
@@ -79,5 +79,62 @@ public class Ejercicio1 {
         }
         media = suma / n;
         System.out.println("La nota media es " + media);
+    }*/
+
+    //------------------------------------------------------------------------------------------------------------------
+
+    //Ejercicio 1 V3
+
+    //Modificar el algoritmo anterior para mostrar además el porcentaje de notas superiores a 10/20.
+    //
+    //Ejemplo de ejecución del algoritmo:
+    //
+    //¿Nota (-1 para terminar)?
+    //
+    //14
+    //
+    //¿Nota (-1 para terminar)?
+    //
+    //12
+    //
+    //¿Nota (-1 para terminar)?
+    //
+    //9
+    //
+    //¿Nota (-1 para terminar)?
+    //
+    //7
+    //
+    //¿Nota (-1 para terminar)?
+    //
+    //10
+    //
+    //¿Nota (-1 para terminar)?
+    //
+    //-1
+    //
+    //Media = 10,4 (60 % ≥10)
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        double nota, suma, media;
+        int n, superiores;
+        Scanner teclado = new Scanner(System.in);
+        suma = 0;
+        n = 0;
+        superiores = 0;
+        System.out.println("¿Nota (-1 para terminar)?");
+        nota = teclado.nextDouble();
+        while (nota != -1) {
+            suma = suma + nota;
+            n = n + 1;
+            if (nota >= 10) {
+                superiores = superiores + 1;
+            }
+            System.out.println("¿Nota (-1 para terminar)?");
+            nota = teclado.nextDouble();
+        }
+        media = suma / n;
+        System.out.println("Media = " + media + " (" + (superiores * 100 / n) + "% ≥10)");
     }
 }
